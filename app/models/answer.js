f65c31a69ade15ba85e8c1d0cd14d33dc2b02ac2
donaldsonjulia <julia.donaldson@gmail.com>
2017-05-12 13:02:39 -0700

    separated answers to be displayed via new c-comment component

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,8 +1,16 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import moment from 'moment';
+
 
 export default DS.Model.extend({
   author: DS.attr('string'),
   createdAt: DS.attr('date'),
   text: DS.attr('string'),
-  question: DS.belongsTo('question')
+  question: DS.belongsTo('question'),
+
+  formattedDate: Ember.computed('createdAt', function() {
+    return moment(this.get('createdAt')).format('MM/DD/YYYY [at] h:mm a ');
+  }),
+
 });
