ce41c7009f38879235e3b8fc5944c2920cfcde12
Julia Donaldson <Julia@Julias-MacBook-Pro.local>
2017-06-03 14:17:48 -0700

    basic mirage configuration with hardcoded data

--- mirage/config.js
+++ mirage/config.js
@@ -1,15 +1,36 @@
 export default function() {
+  this.namespace = '/api';
 
-this.namespace = '/api';
-
-this.get('/questions', () => {
-  return {
-    questions: [
-    {id: 1, author: 'Julia', subject: 'test question', content: "{\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[],\"markups\":[[\"em\"],[\"code\"]],\"sections\":[[1,\"p\",[[0,[],0,\"hello\"]]],[1,\"p\",[[0,[0],1,\"hello\"]]],[1,\"h1\",[[0,[],0,\"hello\"]]],[3,\"ul\",[[[0,[],0,\"hello\"]]]],[3,\"ol\",[[[0,[],0,\" hello hello\"]],[[0,[],0,\"hello\"]]]],[1,\"p\",[]],[1,\"p\",[[0,[1],1,\"this.hello = 'hello!';\"]]]]}", createdAt: '2017-06-03T03:59:38.644Z'}
-    ]
-  }
-
-});
-
-
+  this.get('/questions', function() {
+    return {
+      data: [{
+        type: 'questions',
+        id: 1,
+        attributes: {
+          author: 'Julia',
+          'created-at': '2017-06-03T03:59:38.644Z',
+          subject: 'Test Question',
+          content: "{\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[],\"markups\":[[\"em\"],[\"code\"]],\"sections\":[[1,\"p\",[[0,[],0,\"hello\"]]],[1,\"p\",[[0,[0],1,\"hello\"]]],[1,\"h1\",[[0,[],0,\"hello\"]]],[3,\"ul\",[[[0,[],0,\"hello\"]]]],[3,\"ol\",[[[0,[],0,\" hello hello\"]],[[0,[],0,\"hello\"]]]],[1,\"p\",[]],[1,\"p\",[[0,[1],1,\"this.hello = 'hello!';\"]]]]}"
+        }
+      }, {
+        type: 'questions',
+        id: 2,
+        attributes: {
+          author: 'Lucia',
+          'created-at': '2017-06-03T03:59:38.644Z',
+          subject: 'Test Question 2',
+          content: "{\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[],\"markups\":[[\"em\"],[\"code\"]],\"sections\":[[1,\"p\",[[0,[],0,\"hello\"]]],[1,\"p\",[[0,[0],1,\"hello\"]]],[1,\"h1\",[[0,[],0,\"hello\"]]],[3,\"ul\",[[[0,[],0,\"hello\"]]]],[3,\"ol\",[[[0,[],0,\" hello hello\"]],[[0,[],0,\"hello\"]]]],[1,\"p\",[]],[1,\"p\",[[0,[1],1,\"this.hello = 'hello!';\"]]]]}"
+        }
+      }, {
+        type: 'questions',
+        id: 3,
+        attributes: {
+          author: 'Julia',
+          'created-at': '2017-06-03T03:59:38.644Z',
+          subject: 'Test Question 3',
+          content: "{\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[],\"markups\":[[\"em\"],[\"code\"]],\"sections\":[[1,\"p\",[[0,[],0,\"hello\"]]],[1,\"p\",[[0,[0],1,\"hello\"]]],[1,\"h1\",[[0,[],0,\"hello\"]]],[3,\"ul\",[[[0,[],0,\"hello\"]]]],[3,\"ol\",[[[0,[],0,\" hello hello\"]],[[0,[],0,\"hello\"]]]],[1,\"p\",[]],[1,\"p\",[[0,[1],1,\"this.hello = 'hello!';\"]]]]}"
+        }
+      }]
+    };
+  });
 }
