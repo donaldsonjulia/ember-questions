4c9123f5ded1aefe7ad39246423354558964fccd
Julia Donaldson <Julia@Julias-MacBook-Pro.local>
2017-07-06 23:52:48 -0700

    changed currentUser service 'user' property to fix bug preventing the posting of new questions and answers

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -6,17 +6,11 @@ export default Ember.Service.extend({
   session: service(),
   store: service(),
 
-  user: computed('_user', 'session.isAuthenticated', function() {
-    if (this.get('session.isAuthenticated')) {
-      return this.get('_user');
-    }
-  }),
-
   load() {
     let userId = this.get('session.data.authenticated.user_id');
     if (isPresent(userId)) {
       return this.get('store').findRecord('user', userId).then((user) => {
-        this.set('_user', user);
+        this.set('user', user);
       });
     } else {
       return RSVP.resolve();
