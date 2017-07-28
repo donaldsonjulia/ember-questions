f9c8e1e53ed7a02ec08511e5947ef5da29a92b2f
Julia Donaldson <Julia@Julias-MacBook-Pro.local>
2017-07-28 01:18:54 -0700

    WIP avatar upload controller updates

--- app/controllers/account/avatar.js
+++ app/controllers/account/avatar.js
@@ -1,10 +1,40 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
+const { get, set } = Ember;
 
 export default Ember.Controller.extend({
 
-  actions: {
-    uploadImage() {
+  uploadAvatar: task(function * (image) {
+     let user = this.get('model');
 
+     let avatar = this.store.createRecord('avatar', {
+       user,
+       filename: get(image, 'name'),
+       filesize: get(image, 'size')
+     });
+
+     try {
+       image.readAsDataURL().then(function (url) {
+         if (get(avatar, 'url') === null) {
+           set(avatar, 'url', url);
+         }
+        });
+
+        let response = yield image.upload('/images');
+        set(image, 'url', response.headers.Location);
+        yield avatar.save();
+
+     } catch(e) {
+       avatar.rollbackAttributes();
+       console.log('problem uploading!');
+     }
+
+  }),
+
+  actions: {
+    uploadImage(image) {
+      this.get('uploadAvatar').perform(image);
     }
   }
 });
