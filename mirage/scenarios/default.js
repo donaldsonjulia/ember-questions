be4151513af3857640c8bcb0e237222d5b9a796d
Julia Donaldson <Julia@Julias-MacBook-Pro.local>
2017-07-28 22:56:31 -0700

    added guest user to mirage scenario for demo site login

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -4,11 +4,13 @@ export default function(server) {
   server.create('user', { username: 'Taras', password: 'asdf' }); // 2 user for manual testing
   server.create('user', { username: 'Michael', password: '1234' }); // 3 user for manual testing
   server.create('user', { username: 'Jorge', password: 'jorge123' }); // 4 user for manual testing
+  server.create('user', { username: 'Guest', password: 'password' }); // 4 user for manual testing
 
   server.create('avatar', { userId: 1 });
   server.create('avatar', { userId: 2 });
   server.create('avatar', { userId: 3 });
   server.create('avatar', { userId: 4 });
+  server.create('avatar', { userId: 5 });
 
   server.createList('question', 10, { authorId: 1 });
   server.createList('question', 10, { authorId: 2 });
